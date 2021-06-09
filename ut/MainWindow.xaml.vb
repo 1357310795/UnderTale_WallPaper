fa9e8fdb68d41f1fe6ce327b989de9b13c0f2406
szy <1357310795@qq.com>
2021-06-09 08:41:07 +0800

    添加项目文件。

---
+++ ut/MainWindow.xaml.vb
@@ -0,0 +1,62 @@
+﻿Imports System.IO
+Imports System.Runtime.InteropServices
+Imports System.Text
+Imports System.Windows.Interop
+Imports System.Windows.Threading
+
+Class MainWindow
+
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+        Dim m = Me
+        m.WindowStyle = WindowStyle.None
+        m.Width = 0
+        m.Height = 0
+        m.Top = 0
+        m.Left = 0
+        m.Opacity = 0
+
+        System.Threading.Thread.Sleep(100)
+        Dim workw = GetWorkerW()
+        Console.WriteLine(workw)
+        SetParent((New WindowInteropHelper(m)).Handle, workw)
+        MoveWindow((New WindowInteropHelper(m)).Handle, 0, 0, 1920, 1080, True)
+        m.Opacity = 1
+
+        update()
+        Dim t As New Threading.DispatcherTimer
+        t.Interval = TimeSpan.FromMinutes(1)
+        AddHandler t.Tick, AddressOf update
+        t.Start()
+
+        updatesentence()
+        Dim sentencetimer As New Threading.DispatcherTimer
+        sentencetimer.Interval = TimeSpan.FromMinutes(15)
+        AddHandler sentencetimer.Tick, AddressOf updatesentence
+        sentencetimer.Start()
+
+    End Sub
+
+    Private Sub update()
+        Dim t As TimeSpan = New DateTime(2021, 6, 7, 9, 0, 0) - Now
+        t1.Text = t.Days
+        t2.Text = t.Hours
+        t3.Text = t.Minutes
+    End Sub
+
+    Private Sub updatesentence()
+        Dim path = Environment.CurrentDirectory + "\ttt.txt"
+        Dim sr As New StreamReader(path, Encoding.Default)
+        Dim l As List(Of String) = New List(Of String)
+        Do
+            Dim textContent = sr.ReadLine()
+            If IsNothing(textContent) Then Exit Do
+            l.Add(textContent)
+        Loop While True
+        sr.Close()
+        Dim r As Random = New Random
+        Dim res = l(r.Next(l.Count - 1))
+
+        Text0.Text = res
+    End Sub
+
+End Class
---
+++ ut/Module1.vb
@@ -0,0 +1,54 @@
+﻿Imports System.Runtime.InteropServices
+
+Module Module1
+    <DllImport("user32.dll")>
+    Public Function SendMessageTimeout(ByVal hWnd As IntPtr,
+                                              ByVal wMsg As Integer,
+                                              ByVal wParam As Integer,
+                                              ByVal lParam As Integer,
+                                              ByVal fuFlags As Int32,
+                                              ByVal uTimeout As UInteger,
+                                              <Out> ByRef lpdwResult As Integer) As Integer
+
+    End Function
+    Public Delegate Function EnumMonitorProc(ByVal hMonitor As IntPtr, ByVal hdcMonitor As IntPtr, ByRef rcMonitor As Rect, ByVal data As IntPtr) As Boolean
+    Public Delegate Function EnumWindowsProc(ByVal hwnd As IntPtr, ByVal lParam As IntPtr) As Boolean
+
+    <DllImport("user32")>
+    Public Function EnumWindows(ByVal lpEnumFunc As EnumWindowsProc, ByVal lParam As IntPtr) As Boolean
+
+    End Function
+
+    <DllImport("user32")>
+    Public Function FindWindowEx(ByVal hWndParent As IntPtr, ByVal hWndChildAfter As IntPtr, ByVal lpWindowClass As String, ByVal lpWindowName As String) As IntPtr
+
+    End Function
+
+
+    <DllImport("user32")>
+    Public Function SetParent(ByVal hWndChild As IntPtr, ByVal hWndNewParent As IntPtr) As IntPtr
+
+    End Function
+
+    <DllImport("user32.dll")>
+    Public Function MoveWindow(ByVal handle As IntPtr, ByVal x As Integer, ByVal y As Integer, ByVal nWidth As Integer, ByVal nHeight As Integer, ByVal bRepaint As Boolean) As Boolean
+
+    End Function
+    Public Function GetWorkerW() As IntPtr
+        Dim progmanHandle = FindWindowEx(IntPtr.Zero, IntPtr.Zero, "Progman", Nothing)
+        Dim result = Nothing
+        SendMessageTimeout(progmanHandle, &H52C, 0, 0, 0, 1000, result)
+        Threading.Thread.Sleep(100)
+        Dim workerWHandle = IntPtr.Zero
+        EnumWindows(New EnumWindowsProc(Function(topHandle, topParamHandle)
+                                            Dim shellHandle = FindWindowEx(topHandle, IntPtr.Zero, "SHELLDLL_DefView", Nothing)
+
+                                            If shellHandle <> IntPtr.Zero Then
+                                                workerWHandle = FindWindowEx(IntPtr.Zero, topHandle, "WorkerW", Nothing)
+                                            End If
+
+                                            Return True
+                                        End Function), IntPtr.Zero)
+        Return workerWHandle
+    End Function
+End Module
